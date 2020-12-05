4918adf949da614bdb2820f59065fcf320f4d10b
raphaelattias <raphael.attias@epfl.ch>
2020-12-05 08:14:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ from hyperparameters.select_param import *
 from process_data.import_test import *
 
 
-    if __name__ ==  '__main__':
+if __name__ ==  '__main__':
     # %%
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
