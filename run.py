e7f64450c9e486060fe2021d5109c092ab0be4a5
raphaelattias <raphael.attias@epfl.ch>
2020-12-05 23:21:01 +0100

    Update run.py

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ from hyperparameters.select_param import *
 from process_data.import_test import *
 
 if __name__ ==  '__main__':
+    
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     folder_path_image = 'data/image'
     folder_path_mask  = 'data/mask'
