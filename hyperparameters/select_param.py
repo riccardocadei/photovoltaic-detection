787bb6e71985feffe95b9a2f4d26ab854e5b1cf9
raphaelattias <raphael.attias@epfl.ch>
2020-12-07 10:30:24 +0100

    training tracks loss on val, rewrite of data  loader

--- hyperparameters/select_param.py
+++ hyperparameters/select_param.py
@@ -21,7 +21,7 @@ def seed_torch(seed=0):
     torch.backends.cudnn.deterministic = True
     
 
-def training_model(train_loader,loss_function,optimizer,model,num_epochs,scheduler=None):
+def training_model(train_loader,loss_function,optimizer,model,num_epochs,scheduler=None,val_loader = None):
     """Simple training loop for a model, on a training set, with respect to a loss function and optimizer. The function can take a scheduler
     for the learning rate. 
 
@@ -34,13 +34,26 @@ def training_model(train_loader,loss_function,optimizer,model,num_epochs,schedul
         scheduler (Scheduler, optional): Schedule the learning rate, see pytorch doc. Defaults to None.
 
     Returns:
-        Model: Trained Model
+        list: History of train loss
+        list: History of val loss
+        list: History of train IoU
+        list: History of val IoU
     """
     if scheduler == None:
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 50, gamma=1, last_epoch=-1, verbose=False)
-    
+        
+    history_train_loss = []
+    history_val_loss = []
+    history_train_iou = []
+    history_val_iou = []
+    period = 25
+
     for epoch in range(num_epochs):
+
+        
+        model.train()
         running_train_loss = 0.0
+        running_train_iou = 0.0
         t0 = time.time()
         for i, (images,labels) in enumerate(train_loader):
             if torch.cuda.is_available():
@@ -54,10 +67,34 @@ def training_model(train_loader,loss_function,optimizer,model,num_epochs,schedul
             optimizer.step()
             
             running_train_loss += loss.item()
+            if epoch % period == 0:
+                running_train_iou += iou(np.around((outputs).detach().cpu().numpy()),labels.detach().cpu().numpy())
         scheduler.step()
-        if (epoch % 10) == 0:
-            print('Epoch n.',epoch, 'Train Loss',np.around(running_train_loss/len(train_loader),4),'Time Remaining',np.around((num_epochs-epoch)*(time.time()-t0)/60,4))
-    return model
+
+
+        running_val_loss = 0.0
+        running_val_iou = 0.0
+        if (epoch % period)==0:
+            if (val_loader != None):
+                model.eval()
+                t0 = time.time()
+                for i, (images,labels) in enumerate(val_loader):
+                    if torch.cuda.is_available():
+                                    images=Variable(images.cuda())
+                                    labels=Variable(labels.cuda())
+                    outputs = model(images)
+                    loss = loss_function(torch.squeeze(outputs), torch.squeeze(labels))
+
+                    running_val_loss += loss.item()
+                    running_val_iou += iou(np.around((outputs).detach().cpu().numpy()),labels.detach().cpu().numpy())
+                history_val_loss.append(running_val_loss/len(val_loader))
+                history_val_iou.append(running_val_iou/len(val_loader))
+                print('Epoch n.',epoch, 'Val Loss',np.around(history_val_loss[-1],2),'Time Remaining',np.around((num_epochs-epoch)*(time.time()-t0)/60,1))
+            history_train_loss.append(running_train_loss/len(train_loader))
+            history_train_iou.append(running_train_iou/len(train_loader))
+            print('Epoch n.',epoch, 'Train Loss',np.around(history_train_loss[-1],2),'Time Remaining',np.around((num_epochs-epoch)*(time.time()-t0)/60,1))
+        
+    return history_train_loss, history_val_loss, history_train_iou, history_val_iou
 
 
 def test_model(test_loader,model):
@@ -154,7 +191,7 @@ def cross_validation(train_dataset, n_splits, loss_function,num_epochs,lr):
         
         #train the model
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        model = training_model(train_fold_loader,loss_function,optimizer,model,num_epochs)
+        training_model(train_fold_loader,loss_function,optimizer,model,num_epochs)
         # make prediction and compute the evaluation metrics
         iou, acc = test_model(test_fold_loader,model)
         print('Iter {}: IoU = {:.4} /  Accuracy = {:.4}'.format(fold, iou, acc))
@@ -193,15 +230,14 @@ def adptative_learning(train_dataset, val_loader,loss_function,input_model,num_e
     for lr in lr_candidates:
         print('---------------------------------------------------------------------\n')
         print('Learning Rate = {}\n'.format(lr))
-        iou, acc, model = adptative_helper(train_dataset,val_loader, loss_function, input_model, num_epochs, lr)
-        comparison.append([lr, iou, acc, model])
+        iou, acc = adptative_helper(train_dataset,val_loader, loss_function, input_model, num_epochs, lr)
+        comparison.append([lr, iou, acc])
     comparison = np.array(comparison).reshape(len(lr_candidates),4)
     ind_best =  np.argmax(comparison[:,1]) 
     best_lr = comparison[ind_best,0]
-    best_model = comparison[ind_best,3]
     best_iou = np.max(comparison[:,1])
         
-    return best_model, best_iou, comparison[:,1]
+    return best_iou, comparison[:,1]
 
 def adptative_helper(train_dataset, val_loader, loss_function,input_model,num_epochs,lr):
     
@@ -216,7 +252,7 @@ def adptative_helper(train_dataset, val_loader, loss_function,input_model,num_ep
         
         #train the model
         optimizer = torch.optim.Adam(input_model.parameters(), lr=lr)
-        model = training_model(train_fold_loader,loss_function,optimizer,input_model,num_epochs)
+        training_model(train_fold_loader,loss_function,optimizer,input_model,num_epochs)
         
         # make prediction and compute the evaluation metrics
         iou, acc = test_model(val_loader,model)
@@ -229,4 +265,4 @@ def adptative_helper(train_dataset, val_loader, loss_function,input_model,num_ep
     print("\nAverage validation accuracy: %f" % np.mean(acc_val))
     print("Variance validation accuracy: %f" % np.var(acc_val))
         
-    return np.mean(iou_val), np.mean(acc_val), model
+    return np.mean(iou_val), np.mean(acc_val)
--- run.py
+++ run.py
@@ -18,16 +18,15 @@ from process_data.import_test import *
 
 
 if __name__ ==  '__main__':
-
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    seed_torch()
 
     folder_path_image = 'data/image_residencial'
     folder_path_mask  = 'data/mask_residencial'
     folder_path_noara  = 'data/noARA'
 
     #load dataset
-    dataset = ConcatDataset([DataLoaderSegmentation(folder_path_image,folder_path_mask),DataLoaderNoARA(folder_path_noara)])
+    dataset = ConcatDataset([DataLoaderSegmentation(folder_path_image,folder_path_mask),DataLoaderSegmentation(folder_path_noara)])
+
     #split into train, val, test
     dataset_size = len(dataset)
     train_size = int(0.8*len(dataset))
@@ -40,13 +39,15 @@ if __name__ ==  '__main__':
     val_loader = DataLoader(val_set,batch_size=2, shuffle=True ,num_workers=0)
     test_loader = DataLoader(test_set,batch_size=2, shuffle=True ,num_workers=0)
 
-    num_epochs = 100
-    loss_function = torch.nn.BCEWithLogitsLoss(weight=torch.FloatTensor([7]).cuda())
+
+    num_epochs = 400
+    loss_function = torch.nn.BCEWithLogitsLoss(weight=torch.FloatTensor([6]).cuda())
     model = UNet(3,1,False).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 30, gamma=0.5, last_epoch=-1, verbose=True)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 100, gamma=0.63, last_epoch=-1, verbose=False)
     #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, eta_min=0.01, last_epoch=-1, verbose=True)
 
-    trained_model = training_model(train_loader,loss_function,optimizer,model,num_epochs,scheduler)
-    torch.save(trained_model.state_dict(), 'model/trained_model.pt')
+    history_train_loss, history_val_loss, history_train_iou, history_val_iou = training_model(train_loader,loss_function,optimizer,model,num_epochs,scheduler)
+
+    torch.save(model.state_dict(), 'model/trained_model.pt')
  
