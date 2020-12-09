716307f1dcbcd309047597add943068df1000e26
raphaelattias <raphael.attias@epfl.ch>
2020-12-09 13:15:02 +0100

    update parameters

--- train/train.py
+++ train/train.py
@@ -42,7 +42,7 @@ def training_model(train_loader,loss_function,optimizer,model,num_epochs,schedul
         
         model.train()
         running_train_loss = 0.0
-        running_train_iou = 0.0
+        running_train_iou = []
         t0 = time.time()
         for i, (images,labels) in enumerate(train_loader):
             if torch.cuda.is_available():
@@ -58,13 +58,13 @@ def training_model(train_loader,loss_function,optimizer,model,num_epochs,schedul
             running_train_loss += loss.item()
             if epoch % period == 0:
                 for j in range(images.shape[0]):
-                    iou_j = iou(np.around((outputs[j]).detach().cpu().numpy()),labels[j].detach().cpu().numpy())
-                    running_train_iou += iou_j
+                    iou_j = iou(np.around(sigmoid(outputs[j].detach().cpu().numpy())),labels[j].detach().cpu().numpy())
+                    running_train_iou.append(iou_j)
         scheduler.step()
 
 
         running_val_loss = 0.0
-        running_val_iou = 0.0
+        running_val_iou = []
         model.eval()
         if (epoch % period)==0:
             if (val_loader != None):
@@ -77,13 +77,13 @@ def training_model(train_loader,loss_function,optimizer,model,num_epochs,schedul
 
                     running_val_loss += loss.item()
                     for j in range(images.shape[0]):
-                        iou_j = iou(np.around((outputs[j]).detach().cpu().numpy()),labels[j].detach().cpu().numpy())
-                        running_val_iou += iou_j
+                        iou_j = iou(np.around(sigmoid(outputs[j].detach().cpu().numpy())),labels[j].detach().cpu().numpy())
+                        running_val_iou.append(iou_j)
                 history_val_loss.append(running_val_loss/(len(val_loader)))
-                history_val_iou.append(running_val_iou/(len(val_loader)*2))
-                print('Epoch n.',epoch, 'Val Loss',np.around(history_val_loss[-1],2), 'Val Iou',np.around(history_val_iou[-1],2),'Time Remaining',np.around((num_epochs-epoch)*(time.time()-t0)/60,1))
+                history_val_iou.append(np.mean(running_val_iou))
+                print('Epoch n.',epoch, 'Val Loss',np.around(history_val_loss[-1],2), 'Val Iou',np.mean(running_val_iou))
             history_train_loss.append(running_train_loss/(len(train_loader)))
-            history_train_iou.append(running_train_iou/(len(train_loader)*2))
-            print('Epoch n.',epoch, 'Train Loss',np.around(history_train_loss[-1],2),'Train Iou',np.around(history_train_iou[-1],2),'Time Remaining',np.around((num_epochs-epoch)*(time.time()-t0)/60,1))
+            history_train_iou.append(np.mean(running_train_iou))
+            print('Epoch n.',epoch, 'Train Loss',np.around(history_train_loss[-1],2),'Train Iou',np.mean(running_train_iou),'Time Remaining',np.around((num_epochs-epoch)*(time.time()-t0)/60,1),'min')
             print('----------------------------------------------')
     return history_train_loss, history_val_loss, history_train_iou, history_val_iou
